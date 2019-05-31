66eb0a4e722f03cc0a384708807d2975873a24c6
Apurva-MacbookAir <apurvnakade@gmail.com>
2019-05-31 09:47:21 -0400

    minor edits to scrolling and menu

--- code/scripts/scroll.js
+++ code/scripts/scroll.js
@@ -1,8 +1,8 @@
 //variables for storing the top of the sections statically on page load
 var section_top = new Array();
 var scrollPosition;
-var current_section = 0,
-  new_section = 0;
+var current_section = 0, new_section = 0;
+var menuItems;
 
 //attach event handlers to right and left arrow keys for 'body'
 //move menus to right and left on keydown
@@ -15,7 +15,7 @@ function menuMove(event) {
     }
   } else if (window.event.keyCode == 39 /*left key*/
   || (window.event.keyCode == 32 && !window.event.shiftKey) /*spacebar*/) {
-    if (current_section < 3) {
+    if (current_section < menuItems.length - 1) {
       current_section++;
       $('#menu' + current_section)[0].click();
     }
@@ -25,16 +25,22 @@ function menuMove(event) {
 //event handler for scroll attached to 'body'
 //change the selected menu depending on the scroll height
 function menuSelect() {
+  let sectionSelected = false;
   scrollPosition = $(document).scrollTop();
-  if (scrollPosition <= section_top[1] - 100) {
-    new_section = 0;
-  } else if (scrollPosition <= section_top[2] - 100) {
-    new_section = 1;
-  } else if (scrollPosition <= section_top[3] - 100) {
-    new_section = 2;
-  } else {
-    new_section = 3;
-  }
+
+	menuItems.each(function(index, value){
+		if (scrollPosition <= section_top[index] - 100) {
+		console.log(scrollPosition);
+		console.log(section_top[index]);
+		new_section = index - 1;
+		sectionSelected = true;
+		return false;
+		}
+	});
+	if(!sectionSelected) {
+	new_section = menuItems.length - 1;
+	}
+
   if (new_section != current_section) {
     $("#menu" + current_section).removeClass("menuSelected");
     $("#menu" + new_section).addClass("menuSelected");
@@ -44,12 +50,13 @@ function menuSelect() {
 
 //compute the tops of various divs
 function computeTops() {
-    //store the top of the sections
-    section_top[0] = $("#about").offset().top;
-    section_top[1] = $("#research").offset().top;
-    section_top[2] = $("#teaching").offset().top;
-    section_top[3] = $("#interests").offset().top;
-    menuSelect();
+
+	menuItems = $(".menuItem").not(".unselectable");
+
+	menuItems.each(function(index,value){
+		section_top[index]=$($(this).attr('href')).offset().top;
+	});
+	menuSelect();
 }
 
 $(document).ready(function() {
